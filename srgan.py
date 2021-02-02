52e1a58161468d4cb995f55063da66f9af9bd898
lee <lqy178058008@vip.qq.com>
2021-02-03 00:18:05 +0800

    test

--- srgan.py
+++ srgan.py
@@ -7,7 +7,6 @@ Instrustion on running the script:
 2. Save the folder 'img_align_celeba' to '../../data/'
 4. Run the sript using command 'python3 srgan.py'
 """
-
 import argparse
 import os
 import numpy as np
@@ -32,20 +31,34 @@ os.makedirs("images", exist_ok=True)
 os.makedirs("saved_models", exist_ok=True)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--epoch", type=int, default=0, help="epoch to start training from")
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-parser.add_argument("--dataset_name", type=str, default="img_align_celeba", help="name of the dataset")
-parser.add_argument("--batch_size", type=int, default=4, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
-parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--decay_epoch", type=int, default=100, help="epoch from which to start lr decay")
-parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--hr_height", type=int, default=256, help="high res. image height")
-parser.add_argument("--hr_width", type=int, default=256, help="high res. image width")
-parser.add_argument("--channels", type=int, default=3, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=100, help="interval between saving image samples")
-parser.add_argument("--checkpoint_interval", type=int, default=-1, help="interval between model checkpoints")
+parser.add_argument("--epoch", type=int, default=0,
+                    help="epoch to start training from")
+parser.add_argument("--n_epochs", type=int, default=200,
+                    help="number of epochs of training")
+parser.add_argument("--dataset_name", type=str,
+                    default="img_align_celeba", help="name of the dataset")
+parser.add_argument("--batch_size", type=int, default=4,
+                    help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.0002,
+                    help="adam: learning rate")
+parser.add_argument("--b1", type=float, default=0.5,
+                    help="adam: decay of first order momentum of gradient")
+parser.add_argument("--b2", type=float, default=0.999,
+                    help="adam: decay of first order momentum of gradient")
+parser.add_argument("--decay_epoch", type=int, default=100,
+                    help="epoch from which to start lr decay")
+parser.add_argument("--n_cpu", type=int, default=8,
+                    help="number of cpu threads to use during batch generation")
+parser.add_argument("--hr_height", type=int, default=256,
+                    help="high res. image height")
+parser.add_argument("--hr_width", type=int, default=256,
+                    help="high res. image width")
+parser.add_argument("--channels", type=int, default=3,
+                    help="number of image channels")
+parser.add_argument("--sample_interval", type=int, default=100,
+                    help="interval between saving image samples")
+parser.add_argument("--checkpoint_interval", type=int,
+                    default=-1, help="interval between model checkpoints")
 opt = parser.parse_args()
 print(opt)
 
@@ -75,11 +88,14 @@ if cuda:
 if opt.epoch != 0:
     # Load pretrained models
     generator.load_state_dict(torch.load("saved_models/generator_%d.pth"))
-    discriminator.load_state_dict(torch.load("saved_models/discriminator_%d.pth"))
+    discriminator.load_state_dict(torch.load(
+        "saved_models/discriminator_%d.pth"))
 
 # Optimizers
-optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_G = torch.optim.Adam(
+    generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D = torch.optim.Adam(
+    discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
 
 Tensor = torch.cuda.FloatTensor if cuda else torch.Tensor
 
@@ -102,8 +118,10 @@ for epoch in range(opt.epoch, opt.n_epochs):
         imgs_hr = Variable(imgs["hr"].type(Tensor))
 
         # Adversarial ground truths
-        valid = Variable(Tensor(np.ones((imgs_lr.size(0), *discriminator.output_shape))), requires_grad=False)
-        fake = Variable(Tensor(np.zeros((imgs_lr.size(0), *discriminator.output_shape))), requires_grad=False)
+        valid = Variable(Tensor(
+            np.ones((imgs_lr.size(0), *discriminator.output_shape))), requires_grad=False)
+        fake = Variable(Tensor(
+            np.zeros((imgs_lr.size(0), *discriminator.output_shape))), requires_grad=False)
 
         # ------------------
         #  Train Generators
@@ -160,9 +178,12 @@ for epoch in range(opt.epoch, opt.n_epochs):
             gen_hr = make_grid(gen_hr, nrow=1, normalize=True)
             imgs_lr = make_grid(imgs_lr, nrow=1, normalize=True)
             img_grid = torch.cat((imgs_lr, gen_hr), -1)
-            save_image(img_grid, "images/%d.png" % batches_done, normalize=False)
+            save_image(img_grid, "images/%d.png" %
+                       batches_done, normalize=False)
 
     if opt.checkpoint_interval != -1 and epoch % opt.checkpoint_interval == 0:
         # Save model checkpoints
-        torch.save(generator.state_dict(), "saved_models/generator_%d.pth" % epoch)
-        torch.save(discriminator.state_dict(), "saved_models/discriminator_%d.pth" % epoch)
+        torch.save(generator.state_dict(),
+                   "saved_models/generator_%d.pth" % epoch)
+        torch.save(discriminator.state_dict(),
+                   "saved_models/discriminator_%d.pth" % epoch)
